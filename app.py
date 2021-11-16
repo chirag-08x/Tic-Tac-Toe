045db0cf73701a33dd69a922dc3a032b43b035a3
chirag-08x <cagrawal081200@gmail.com>
2021-11-16 23:23:06 +0530

    diagnols done with conditional statements

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def printBoard():
 
 def playTicTacToe():
     i = 1
-    entries = [0, 1]
     while i <= 9:
         randomRow = random.randint(0, 2)
         randomCol = random.randint(0, 2)
@@ -23,7 +22,6 @@ def playTicTacToe():
             board[randomRow][randomCol] = 1
         else:
             board[randomRow][randomCol] = 2
-            
         i += 1
         printBoard()
         winner = checkForWinner()
@@ -32,17 +30,34 @@ def playTicTacToe():
     print("Match has drawn.")
 
 def checkForWinner():
-    for item in board:
-        check = item[0] == item[1] == item[2] and item[0] == item[1] == item[2] != 0
+    for index,item in enumerate(board):
+        # Check for similar number in rows.
+        rowItems = {item[0], item[1], item[2]}
+        check = len(rowItems) == 1 and 0 not in rowItems
         if check:
             print(f"Player {item[0]} has won.")
             return True
 
-    for item in range(len(board)):
-        check = board[0][item] == board[1][item] == board[2][item] and board[0][item] == board[1][item] == board[2][item] != 0
+        # checking for similar number in columns.
+        colItems = {board[0][index], board[1][index],board[2][index]}
+        check = len(colItems) == 1 and 0 not in colItems
         if check:
-            print(f"Player {board[0][item]} has won.")
+            print(f"Player {board[0][index]} has won.")
             return True
+
+        leftDiagnol = {board[0][0], board[1][1], board[2][2]}
+        if len(leftDiagnol) == 1 and 0 not in leftDiagnol:
+            print(f"Player {board[1][1]} has won.")
+            return True
+
+        rightDiagnol = {board[0][2], board[1][1], board[2][0]}
+        if len(rightDiagnol) == 1 and 0 not in rightDiagnol:
+            print(f"Player {board[1][1]} has won.")
+            return True
+
     return False
 
 playTicTacToe()
+
+# Diagnols ==> Right Diagnol  ===> [0][0], [1][1], [2][2]
+#              Left Diagnol   ===> [0][2], [1][1], [2][0]
